f332cafc381726f235edfccc9d750dc019b5dfe5
luritas <luritas.me@gmail.com>
2018-08-13 10:54:59 +0900

    fix domain name

--- validator.sh
+++ validator.sh
@@ -5,10 +5,9 @@ SSL=`openssl dgst -sha256 -hex -sign ~/ssl/_dotorie_com_private.key <(echo ${KEY
 IFS=' ' read -a SSL_SIGN <<< "${SSL}"
 
 DOMAIN=${1}
-DOMAIN="s2.dotorie.com"
 SIGN=`/opt/ripple/bin/validator-keys --keyfile ~/.ripple/validator-keys.json sign ${DOMAIN}`
 
-echo -e "DOMAI\n${DOMAIN}"
+echo -e "DOMAIN\n${DOMAIN}"
 echo ""
 echo -e "Validator Public Key\n${KEY}"
 echo ""
